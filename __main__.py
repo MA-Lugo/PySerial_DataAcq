8795cb30bcaed63fefe13ea2fee0ea0d6d3e1d12
Mario A. Lugo <mario.luggar@gmail.com>
2021-01-29 20:56:26 -0600

    feat(serial): Implement serial communication
    Functions implemented for handling serial communication
    and print on terminal the input data.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
-from time import time
+import time
+import threading
 from colors import * 
 from tkinter import *
 from tkinter import ttk
+import serial
 
 
 
@@ -11,8 +13,84 @@ GUI.configure(bg = bg_color)
 GUI.geometry('470x186')
 GUI.resizable(width=False, height=False)
 
-def run():
-    pass
+
+connection_status = False
+filtered_data = ""
+sample_rate = 0.05 # 50 ms
+
+def ConnectionEnd():
+    global connection_status
+    control_button.config(text = "R U N")
+    control_button.config(bg = blue_color)
+    connection_status = False
+
+def ConnectionStart():
+    global connection_status
+    control_button.config(text = "S T O P")
+    control_button.config(bg = red_color)
+    connection_status = True
+
+
+
+def CTRLButton_Handle():
+    global connection_status
+    global serial_object
+
+    if (connection_status == False):
+        Try_Connection()
+    else:
+        ConnectionEnd()
+        serial_object.close()
+
+
+
+def Try_Connection():
+    global connection_status 
+    global serial_object
+    port = input_port.get()
+    speed = speed_port.get()
+
+    try:
+        serial_object = serial.Serial( str(port), baudrate= speed, timeout = 1)
+        ConnectionStart()
+    except:
+        
+        print ("Cant Open Specified Port")
+        ConnectionEnd()
+
+
+def SerialDataAcq():
+    global connection_status
+    global serial_object
+    global filtered_data
+
+    while(1):
+        time.sleep(0.01) #delay for do not overload the CPU
+        while(connection_status):
+            try:
+                serial_input = serial_object.readline()
+                filtered_data = serial_input
+            except:
+                print("Connection Error")
+
+def UpdateGUI():
+    global connection_status
+    global filtered_data
+    while(1):
+        time.sleep(0.01)#delay for do not overload the CPU
+        while(connection_status):
+            time.sleep(sample_rate/2)
+            print(filtered_data)
+            
+    
+
+myThread1 = threading.Thread(target = UpdateGUI)
+myThread1.daemon = True
+myThread1.start()   
+
+myThread2 = threading.Thread(target = SerialDataAcq)
+myThread2.daemon = True
+myThread2.start()   
 
 #FRAMES
 item1  = Frame(height = 130, width = 150, bd = 2, relief = 'groove',bg =bg_color)
@@ -46,11 +124,11 @@ item_name3.place(x = 80+155+155, y = 20, anchor = "center")
 
 
 input_port = Entry(widt=12,font =("", 10),justify= "center",bg = gray_color,fg = label_color)
-input_port.insert(INSERT,"COM4")
+input_port.insert(INSERT,"COM3")
 input_port.place(x = 80+155, y= 155,anchor = "center")
 
 speed_port = Entry(widt=12,font =("", 10),justify="center",bg = gray_color,fg = label_color)
-speed_port.insert(INSERT,"9600")
+speed_port.insert(INSERT,"19200")
 speed_port.place(x = 80+155+155, y= 155,anchor ="center")
 
 
@@ -89,8 +167,13 @@ Label(textvariable = input2,font =("", 25),bg = bg_color,fg = label_color).place
 
 
 #BUTTONS
-Button(text = "R U N",font = ("",7), command = run, width = 12,bg=red_color,fg =label_color).place(x = 80, y = 155,anchor="center")
+
+control_button = Button(text = "R U N",font = ("",7),width = 12,command = CTRLButton_Handle,bg=blue_color,fg =label_color)
+control_button.place(x = 80, y = 155,anchor="center")
 
 if __name__ == '__main__':
     
     GUI.mainloop()
+
+    
+    
